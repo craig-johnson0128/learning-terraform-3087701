a92f94bbd8623cfc8bb4612a4008390355239413
Craig Johnson <70026201+craig-johnson0128@users.noreply.github.com>
2023-12-21 21:01:14 -0500

    ALB format

--- main.tf
+++ main.tf
@@ -38,12 +38,12 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.3.1"
   
-  name    = blog
+  name     = "blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.alb.target_groups_arns
+  target_group_arns = module.alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
@@ -98,7 +98,6 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
