1f7f39287e885aac131ab25983b09343f01cfe28
Craig Johnson <70026201+craig-johnson0128@users.noreply.github.com>
2023-12-21 20:42:22 -0500

    update target id

--- main.tf
+++ main.tf
@@ -94,12 +94,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id.       = aws_instance.blog.id
     }
   }
 
