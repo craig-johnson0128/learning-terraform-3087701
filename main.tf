ad06c217b81bfbc87cace208b8e67cbc80b3f31b
Craig Johnson <70026201+craig-johnson0128@users.noreply.github.com>
2023-12-21 20:43:05 -0500

    format

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id.       = aws_instance.blog.id
+      target_id       = aws_instance.blog.id
     }
   }
 
